85d990f70e1a0cc4459f58b33af8af87e58aa51f
天航 李 <1197044686@qq.com>
2021-10-13 11:13:27 +0800

    navigation

--- routers/route.go
+++ routers/route.go
@@ -10,6 +10,7 @@ func SetRouters() *gin.Engine {
 	r := gin.Default()
 	//告诉gin框架在哪里找静态文件
 	r.Static("/layui", "static/layui")
+	r.Static("/css", "static/css")
 	r.Static("/acm/layui", "static/layui")
 	//告诉gin框架在哪里找hmtl文件
 	r.Static("/img", "img")
